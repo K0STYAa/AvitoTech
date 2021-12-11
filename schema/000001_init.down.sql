2400049cf3891ef351de300f0ebb2847e3811d2c
K0STYAa <kkukushk@gmail.com>
2021-12-12 00:30:09 +0300

    add indexes

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,3 +1,9 @@
+DROP INDEX s_r_id;
+
+DROP INDEX amount;
+
+DROP INDEX departure_time;
+
 DROP TABLE history;
 
 DROP TABLE users;
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,14 +1,18 @@
 CREATE TABLE users
 (
-    id            serial              CHECK (id >= 0) unique,
+    id            serial PRIMARY KEY  CHECK (id >= 0) unique,
     balance       int                 CHECK (balance >= 0)
 );
 
 CREATE TABLE history
 (
-    id             serial                                           not null unique,
+    id             serial PRIMARY KEY                               not null unique,
     sender_id      int references users (id) on delete cascade      CHECK (sender_id >= 0),
     receiver_id    int references users (id) on delete cascade      CHECK (receiver_id >= 0),
     amount         int                                              CHECK (amount > 0),
     departure_time timestamp                                        not null
 );
+
+CREATE INDEX s_r_id ON history(sender_id, receiver_id);
+CREATE INDEX amount ON history(amount);
+CREATE INDEX departure_time ON history(departure_time);
